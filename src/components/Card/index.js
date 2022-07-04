94b85e7452c5d3cf7133595cdef486e3ac3be570
Luis Lozada <luislofra460@gmail.com>
2022-07-03 23:44:20 -0400

    Eliminacion de comentarios en el componente Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,20 +1,8 @@
-import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { getUrlImgCard } from "../../helpers";
 
 export default function Card({ card, ...props }) {
   const formatImg = card.CardType === "HQ" || card.Rarity === "Common" ? "png" : "gif";
-  // const imgRef = useRef(null);
-  
-  // useEffect( () => {
-  //   if (imgRef.current) {
-  //     if (imgRef.current.complete) {
-  //       console.log("cargo");
-  //     }else {
-  //       console.log("no cargo");
-  //     }
-  //   }
-  // }, [imgRef]);
 
   return (
     <Link to={`/details/${card.id}`} className="card">
@@ -23,7 +11,6 @@ export default function Card({ card, ...props }) {
         className="card__img"
         src={getUrlImgCard(card.id, formatImg)}
         alt={card.Name}
-        // ref={imgRef}
       />
     </Link>
   );
